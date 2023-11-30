0e5c421fea8e50bfa07ef224463313f6b5261099
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2023-11-30 14:20:45 +0300

    Update kind.sh

--- kind.sh
+++ kind.sh
@@ -4,7 +4,7 @@
 #docker install
 
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
