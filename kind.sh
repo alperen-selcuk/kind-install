c53819272e8c065df17102bdacf83db3248853ad
Hasan Alperen SELÇUK <78741582+alperen-selcuk@users.noreply.github.com>
2023-11-30 14:24:47 +0300

    Update kind.sh

--- kind.sh
+++ kind.sh
@@ -24,14 +24,26 @@ systemctl enable docker
 
 #kubectl install
 
+#!/bin/bash
+
+#ubuntu üzerine kubectl kurulumu
+
 sudo apt-get update
-sudo apt install apt-transport-https curl -y
+sudo apt-get install -y apt-transport-https ca-certificates curl
 
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 
-sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt-get update
 
-sudo apt install kubeadm kubelet kubectl kubernetes-cni -y
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+sudo curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+
+sudo chmod 700 get_helm.sh
+
+sudo ./get_helm.sh
 
 #kind binary install
 
